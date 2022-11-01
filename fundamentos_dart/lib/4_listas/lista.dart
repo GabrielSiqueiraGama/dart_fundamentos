58fb8ba86e99c12ebe38aef0425896eaa8a0ce87
Gabriel Siqueira Gama <Siqueiragamagabriel@gmail.com>
2022-11-01 12:19:32 -0300

    listas

---
+++ fundamentos_dart/lib/4_listas/lista.dart
@@ -0,0 +1,8 @@
+// ignore_for_file: unused_local_variable
+
+void main() {
+  List<int> lista = [1, 2, 3];
+  List<String> nomes = ['Gabriel', 'Carlos', 'Jarder'];
+  var numeros = [1, 2, 2, 4];
+  var listaInteiros = <int>[];
+}
